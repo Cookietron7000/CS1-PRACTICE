25f1d26b5c00c91666407692d362a0135f0cea9c
Cookietron7000 <55599915+Cookietron7000@users.noreply.github.com>
2019-09-20 13:31:27 -0500

    Add files via upload

---
+++ test.py
@@ -0,0 +1,10 @@
+if 3 == 3:
+    print('math had not failed us')
+    print("three still equals three")
+print("The if block is over")
+
+answer = int(input("enter a number from 1 to 10"))
+if answer >= 8:
+                   print("GOOD")
+if answer <= 3:
+    print("Bad")
